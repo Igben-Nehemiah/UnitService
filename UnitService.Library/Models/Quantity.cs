bc7315c552bdd38ff7ae4187ed9d2493c6cb2d19
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-02 20:35:25 +0100

    Created Quantity model

---
+++ UnitService.Library/Models/Quantity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UnitService.Library.Models
+{
+    public class Quantity
+    {
+        public Quantity(double? value, Unit unit)
+        {
+            CurrentUnit = unit;
+            Value = value;
+        }
+
+        public Unit CurrentUnit { get; set; }
+        public double? Value { get; set; }
+        public string Dimensionality => CurrentUnit.Dimensionality;
+
+        public Quantity To(Unit unit)
+        {
+            // Check if dimensions are consistent
+            if (!CurrentUnit.HasSameDimensionAs(unit)) throw new Exception();
+
+            throw new NotImplementedException();
+        }
+
+
+        public static Quantity operator *(Quantity quantity, double number)
+        {
+            return new Quantity(quantity.Value.GetValueOrDefault() * number,
+                quantity.CurrentUnit);
+        }
+
+        public static Quantity operator *(double number, Quantity quantity) => quantity * number;
+        
+        public Quantity ToBaseUnit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString() => $"Quantity(value:{Value}, unit:{CurrentUnit})";
+    }
+}
