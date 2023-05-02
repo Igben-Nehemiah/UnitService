d1b785d8a7aefdf268d13a4257a37bca96a77cce
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-02 20:35:42 +0100

    Created simple implementation of the unit model

---
+++ UnitService.Library/Models/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UnitService.Library.Models
+{
+    public class Unit
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Dimensionality { get; set; }
+
+        public bool HasSameDimensionAs(Unit unit) => Dimensionality == unit.Dimensionality;
+
+        public static Unit operator *(Unit left, Unit right)
+        {
+            //return new Unit();
+            throw new NotImplementedException();
+        }
+
+        public static Quantity operator *(double number, Unit unit) => new Quantity(number, unit);
+    }
+}
