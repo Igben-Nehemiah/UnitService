24a1e4e8aa2a1d537e2eeb2c33dfc5ff8bdede9f
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-02 20:36:45 +0100

    created basic implementation of unit registry

---
+++ UnitService.Library/Models/UnitRegistry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UnitService.Library.Models
+{
+    public class UnitRegistry
+    {
+        private readonly UnitRegistryOptions _opts;
+
+        public UnitRegistry(UnitRegistryOptions opts)
+        {
+            _opts = opts;
+        }
+        public void RegisterUnit(string name, Unit unit)
+        {
+
+        }
+
+        public Unit GetUnit(string unitName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
