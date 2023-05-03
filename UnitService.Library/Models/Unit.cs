7d5e3431b9ea023945ea367ca548f6f591d0203e
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-03 23:26:37 +0100

    implemented IEquatable interface

--- UnitService.Library/Models/Unit.cs
+++ UnitService.Library/Models/Unit.cs
@@ -2,14 +2,21 @@
 
 namespace UnitService.Library.Models
 {
-    public class Unit
+    public class Unit : IEquatable<Unit>
     {
         public string Name { get; set; }
         public string Symbol { get; set; }
         public string Dimensionality { get; set; }
+        public Unit(string name, string symbol, string dimensionality)
+        {
+            Name = name;
+            Symbol = symbol;
+            Dimensionality = dimensionality;    
+        }
 
         public bool HasSameDimensionAs(Unit unit) => Dimensionality == unit.Dimensionality;
 
+        
         public static Unit operator *(Unit left, Unit right)
         {
             //return new Unit();
@@ -17,5 +24,14 @@ namespace UnitService.Library.Models
         }
 
         public static Quantity operator *(double number, Unit unit) => new Quantity(number, unit);
+
+        #region Equality
+        public bool Equals(Unit other)
+        {
+            return Name == other.Name
+                && Symbol == other.Symbol
+                && Dimensionality == other.Dimensionality;
+        }
+        #endregion
     }
 }
