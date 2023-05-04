17e814c420f90ee3d45094452fe52773e0bea562
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-04 22:57:45 +0100

    created dimensions constants

---
+++ UnitService.Library/Constants/Dimensions.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitService.Library.Constants
+{
+    public static class Dimensions
+    {
+        public const string LENGTH = "[Length]";
+        public const string TIME = "[Time]";
+        public const string MASS = "[Mass]";
+        public const string CURRENT = "[Current]";
+        public const string TEMPERATURE = "[Temperature]";
+    }
+}
