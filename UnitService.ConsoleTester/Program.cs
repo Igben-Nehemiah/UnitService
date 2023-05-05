77e7e9f45009086e86d17623e305634be27c96c3
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-05 06:35:20 +0100

    added basic test for dimension

--- UnitService.ConsoleTester/Program.cs
+++ UnitService.ConsoleTester/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("This is for testing the unit service");
+﻿using UnitService.Library.Models;
+
+// Will have to define fraction later
+
+
+Dimension dimension= new Dimension();
+dimension.LengthExp = 1;
+dimension.TimeExp = -1;
+
+var t = Dimension.Parse("[Length]^3[Mass][Time]^(-1/3)");
+
+Console.WriteLine(dimension.ToString());
