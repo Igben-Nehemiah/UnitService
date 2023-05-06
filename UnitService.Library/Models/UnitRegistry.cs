119d5404ce3ed317b751af1354bcefa5203a7326
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-06 15:23:37 +0100

    Made class static

--- UnitService.Library/Models/UnitRegistry.cs
+++ UnitService.Library/Models/UnitRegistry.cs
@@ -4,26 +4,26 @@ using UnitService.Library.Constants;
 
 namespace UnitService.Library.Models
 {
-    public class UnitRegistry
+    public static class UnitRegistry
     {
-        private readonly UnitRegistryOptions _opts;
-        private readonly Dictionary<string, Unit>
+        private static readonly UnitRegistryOptions _opts;
+        private static readonly Dictionary<string, Unit>
             units = new Dictionary<string, Unit>();
 
-        public UnitRegistry(UnitRegistryOptions opts)
+        static UnitRegistry()
         {
-            _opts = opts;
+            _opts = new UnitRegistryOptions();
             units.Add("METER", new Unit("METER", "m", (1, 0), Dimensions.LENGTH));
             units.Add("KILOMETER", new Unit("KILOMETER", "km", (0.001, 0), Dimensions.LENGTH));
             units.Add("SECOND", new Unit("SECOND", "s", (1, 0), Dimensions.LENGTH));
             units.Add("METER_PER_SECOND", new Unit("METER_PER_SECOND", "m/s", (1, 0), Dimensions.LENGTH));
         }
-        public void RegisterUnit(string name, Unit unit)
+        public static void RegisterUnit(string name, Unit unit)
         {
             units.Add(name, unit);
         }
 
-        public Unit GetUnit(string unitName)
+        public static Unit GetUnit(string unitName)
         {
             try
             {
