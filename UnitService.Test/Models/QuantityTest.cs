0134f0de2ce9ac3bc2832c74198d3772855e3f40
Obomaese Igben <igbennehemiah@gmail.com>
2023-05-06 23:57:36 +0100

    created simple tests for quantity

---
+++ UnitService.Test/Models/QuantityTest.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitService.Test.Models
+{
+    public class QuantityTest
+    {
+        private readonly string METER = "METER";
+        private readonly string KILOMETER = "KILOMETER";
+
+        [Fact]
+        public void WhenConvertToBaseUnitIsCalledOnNonBaseUnit_ShouldPerformConversion()
+        {
+            var kilometerUnit = UnitRegistry.GetUnit(KILOMETER);
+            Quantity lengthInKilometers = new(1, kilometerUnit);
+
+            var lengthInBaseUnit = lengthInKilometers.ConvertToBaseUnit();
+
+            Assert.True(lengthInBaseUnit.Magnitude == 1000 &&
+                lengthInBaseUnit.Unit == UnitRegistry.GetBaseUnit(kilometerUnit.Dimension));
+        }
+
+        [Fact]
+        public void WhenConvertToIsCalledWithValidUnit_ShouldPerformConversion()
+        {
+            var meterUnit = UnitRegistry.GetUnit(METER);
+            var kilometerUnit = UnitRegistry.GetUnit(KILOMETER);
+            Quantity lengthInMeters = new Quantity(1000, meterUnit);
+
+            var lengthInKilometers = lengthInMeters.ConvertTo(kilometerUnit);
+
+            Assert.True(lengthInKilometers.Magnitude == 1 &&
+                lengthInKilometers.Unit == kilometerUnit);
+
+        }
+    }
+}
