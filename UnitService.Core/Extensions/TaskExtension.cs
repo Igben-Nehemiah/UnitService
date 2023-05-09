8f4b8ab1a07f62b923fbffb83a802ac2ec10ef36
Nehemiah-Igben <nehemiah.igben@cyphercrescent.com>
2023-05-09 14:15:07 +0100

    added utility class for task

---
+++ UnitService.Core/Extensions/TaskExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UnitService.Core.Extensions
+{
+    internal static class TaskExtension
+    {
+        public async static void Await(this Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
